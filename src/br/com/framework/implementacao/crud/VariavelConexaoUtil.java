5ebb66bfb05305920bca0909a6aaf2858075774b
Carlos Daniel <danielbarboza942@gmail.com>
2020-09-14 08:15:20 -0300

    first commit

---
+++ src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,10 @@
+package br.com.framework.implementacao.crud;
+/*
+ * Nome do caminho JNDI datasource Tomcat
+ * @author Carlos
+ */
+public class VariavelConexaoUtil {
+	
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+	
+}
